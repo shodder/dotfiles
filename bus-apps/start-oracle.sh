2ecc135e8280843466601c76f9376ff7b9332293
Simon Hodder <2083432+shodder@users.noreply.github.com>
2023-02-21 20:56:29 +0000

    clean up dots

--- bus-apps/start-oracle.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-function oracle {
-	pwsh & '/mnt/c/Users/mhr39349/programs/oracle-developer/sqldeveloper.exe'
-}
--- java/maven-project-creator.sh
+++
@@ -1,4 +0,0 @@
-mvn-create-project() {
-	mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4
-}
-
